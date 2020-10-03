c8f750db9a57b0cf7025219643453f0c524bd1cd
Matheus <matheusfrancoalves@hotmail.com>
2020-10-03 11:29:49 -0300

    context API (signIn/signOut) and implement private routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,21 +1,22 @@
 import React from 'react';
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 
+import Route from './Route';
+
+import SignIn from '../pages/SignIn';
+
+/*
 import Dashboard from '../pages/Dashboard';
 import Import from '../pages/Import';
 import New from '../pages/New';
 import TransactionDetailed from '../pages/TransactionDetailed';
 import Chart from '../pages/Chart';
+*/
 
 const Routes: React.FC = () => (
   <Switch>
-    <Route path="/graphs" component={Chart} />
-
-    <Route path="/" exact component={Dashboard} />
-    <Route path="/import" component={Import} />
-    <Route path="/new" component={New} />
-    <Route path="/:id" component={TransactionDetailed} />
+    <Route path="/" exact component={SignIn} />
   </Switch>
 );
 
