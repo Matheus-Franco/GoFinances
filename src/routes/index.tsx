c86567d9cab030e5992db4699d668afbcbe664ee
Matheus <matheusfrancoalves@hotmail.com>
2020-10-04 11:01:16 -0300

    create a new transaction

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,9 +8,9 @@ import SignIn from '../pages/SignIn';
 
 import Dashboard from '../pages/Dashboard';
 import TransactionDetailed from '../pages/TransactionDetailed';
+import New from '../pages/New';
 /*
 import Import from '../pages/Import';
-import New from '../pages/New';
 import Chart from '../pages/Chart';
 */
 
@@ -19,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/new" component={New} isPrivate />
     <Route path="/:id" component={TransactionDetailed} isPrivate />
   </Switch>
 );
