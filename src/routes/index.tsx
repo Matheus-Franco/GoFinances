05e0aae69f20a23ebdaa7c375f8adcbf3a019ec8
Matheus <matheusfrancoalves@hotmail.com>
2020-10-04 10:56:24 -0300

    change endpoint to navigate to details

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,10 @@ import Route from './Route';
 import SignIn from '../pages/SignIn';
 
 import Dashboard from '../pages/Dashboard';
+import TransactionDetailed from '../pages/TransactionDetailed';
 /*
 import Import from '../pages/Import';
 import New from '../pages/New';
-import TransactionDetailed from '../pages/TransactionDetailed';
 import Chart from '../pages/Chart';
 */
 
@@ -19,6 +19,7 @@ const Routes: React.FC = () => (
     <Route path="/" exact component={SignIn} />
 
     <Route path="/dashboard" component={Dashboard} isPrivate />
+    <Route path="/:id" component={TransactionDetailed} isPrivate />
   </Switch>
 );
 
